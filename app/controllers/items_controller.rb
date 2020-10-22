8391608c1ed9fc442e4097fed713f082abfb5256
Asuka217 <asuka375@gmail.com>
2020-10-22 17:43:40 +0900

    商品出品ができる

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,24 @@
 class ItemsController < ApplicationController
+
   def index
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:title, :explication, :price, :image, :category_id, :item_status_id, :fee_shipping_id, :region_id, :days_to_ship_id).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,28 @@
 class Item < ApplicationRecord
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :category, :item_status, :fee_shipping, :region, :days_to_ship
+
   has_one_attached :image
-  
-  validates: :title, :explication, :price, :category, :item_status, :fee_shipping, :region, :days_to_ship, presence: true
-  validates: :category_id, numericality: { other_than: 1 }
-  validates: :item_status_id, numericality: { other_than: 1 }
-  validates: :fee_shipping_id, numericality: { other_than: 1 }
-  validates: :region_id, numericality: { other_than: 1 }
-  validates: :days_to_ship_id, numericality: { other_than: 1 }
+  belongs_to :user
+
+  validates :image, presence: true
+  validates :title, presence: true
+  validates :explication, presence: true
+  validates :price, presence: true, inclusion: { in: 300..9999999 }
+  validates :category, presence: true
+  validates :item_status, presence:true
+  validates :fee_shipping, presence: true
+  validates :region, presence: true
+  validates :days_to_ship, presence: true
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :item_status
+  belongs_to_active_hash :fee_shipping
+  belongs_to_active_hash :region
+  belongs_to_active_hash :days_to_ship
+
+  validates :category_id, numericality: { other_than: 1 }
+  validates :item_status_id, numericality: { other_than: 1 }
+  validates :fee_shipping_id, numericality: { other_than: 1 }
+  validates :region_id, numericality: { other_than: 1 }
+  validates :days_to_ship_id, numericality: { other_than: 1 }
 end
