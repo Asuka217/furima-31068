aa71ac664c53e10b9fed817e74680fcc9c11d4b6
Asuka217 <asuka375@gmail.com>
2020-10-26 16:17:34 +0900

    Itemsコントローラーの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -39,7 +39,7 @@ class ItemsController < ApplicationController
 
   def destroy
     if current_user.id == @item.user_id
-      Item.destroy(params[:id])
+      @item.destroy
       redirect_to root_path
     end
   end
